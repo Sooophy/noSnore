f8782f0ba140efe20b2b365fc787ec95340d945d
Sophie <eggtart11@163.com>
2024-12-18 19:43:47 -0500

    Initial Commit

---
+++ noSnore/noSnoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  noSnoreApp.swift
+//  noSnore
+//
+//  Created by Sophie on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct noSnoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
